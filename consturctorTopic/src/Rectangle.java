6f6063f03adf438b3a1266eb92f4c72fd609b8c3
sravanthi-chepuri93 <sravanthi.chepuri93@gmail.com>
2023-06-26 17:29:39 -0400

    rectangle class with constructor and getter and setter methods

---
+++ consturctorTopic/src/Rectangle.java
@@ -0,0 +1,46 @@
+public class Rectangle {
+    private double length;
+    private double breadth;
+    public Rectangle(){
+        length =1;
+        breadth =1;
+    }
+    public Rectangle(double l, double b){
+        setLength(l);
+        setBreadth(b);
+    }
+    public Rectangle(double s){
+        length = breadth= s;
+    }
+    public void setLength(double l){
+        if(l>0)
+            length = l;
+        else
+            length =0;
+    }
+    public void setBreadth(double b){
+        if(b>0)
+            breadth = b;
+        else
+            breadth =0;
+    }
+    public double getLength(){
+        return length;
+    }
+    public double getBreadth(){
+        return breadth;
+    }
+
+
+}
+
+class RectangleTest{
+    public static void main(String[] args){
+        Rectangle r = new Rectangle(-10,5);
+        System.out.println(r.getLength());
+        System.out.println(r.getBreadth());
+
+
+    }
+
+}
